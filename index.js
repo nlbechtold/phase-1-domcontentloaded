b0c4940fdb1d8aaf1405bc8f39a0269e5f5d13f3
Nancy Bechtold <nancyleebechtold@gmail.com>
2024-06-10 14:28:21 -0600

    domediting

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Your code goes here
+// const pSel = document.querySelector("p")
+// pSel.document.textContent = "This is really cool"
+
+// const event1 = document.querySelector("#text") 
+
+
+const event = document.addEventListener("DOMContentLoaded", (event)=> {
+        const textChange = document.querySelector("#text");
+        textChange.textContent = "This is really cool!";
+       
+
+    }
+  
+)
+// const textChange = document.querySelector("#text p");
+// p.document.textContent ="This is really cool!"
+// const pSel = document.querySelector("#text")
+//     pSel.document.textContent = "This is really cool!"
+
+console.log("The DOM has loaded");
